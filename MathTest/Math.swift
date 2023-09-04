05ad4fe2cabfdb4f40c2bb1274c925d43c3f6b65
= <=>
2023-09-04 15:18:10 +0300

    Math UnitTest

---
+++ MathTest/Math.swift
@@ -0,0 +1,27 @@
+//
+//  Math.swift
+//  MathTest
+//
+//  Created by Mert Deniz Akbaş on 4.09.2023.
+//
+
+import Foundation
+
+
+class Math {
+    func sum(a: Int, b:Int) -> Int {
+        return a + b
+    }
+    
+    func mul(a: Int, b: Int) -> Int {
+        return a * b
+    }
+    
+    func div(a: Int, b: Int) -> Int {
+        return a / b
+    }
+    
+    func subs(a: Int, b: Int) -> Int {
+        return a - b
+    }
+}
